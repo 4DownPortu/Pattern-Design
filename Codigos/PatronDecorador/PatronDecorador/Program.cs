aebfa81750b894c5c63293635297be0e56229127
Adolfo Portugal Quintero <47735281+4DownPortu@users.noreply.github.com>
2021-10-26 16:09:22 -0700

    Avance inicial
    Primer avance de proyecto.

--- Codigos/PatronDecorador/PatronDecorador/Program.cs
+++ Codigos/PatronDecorador/PatronDecorador/Program.cs
@@ -6,6 +6,68 @@ using System.Threading.Tasks;
 
 namespace PatronDecorador
 {
+    public class cafeteria
+    {
+        public void Ticket()
+        {
+            
+        }
+        public void Condimentos()
+        {
+            Console.WriteLine("---MENU CONDIMENTOS---");
+        }
+        public void Menu()
+        {
+            int cafe, desicion;
+            double costo;
+
+            Console.Clear();
+            Console.WriteLine("Bienvenido a la cafeteria PortuCafe");
+            Console.WriteLine("¿Que desea tomar el dia de hoy?\n\n");
+
+            Console.WriteLine("---MENU---");
+            Console.WriteLine("1. House Blend");
+            Console.WriteLine("2. Mocha");
+            Console.WriteLine("3. Expresso");
+
+            cafe = Convert.ToInt32(Console.ReadLine());
+            desicion = Convert.ToInt32(Console.ReadLine());
+
+            switch (cafe)
+            {
+                case 1:
+                    Console.WriteLine("Excelente desicion tu cafe es un House Blend");
+                    costo = 50;
+
+                    Console.WriteLine("¿Gusta algun condimento?");
+                    Console.WriteLine("1. Si");
+                    Console.WriteLine("2. No");
+
+                    if (desicion == 1)
+                    {
+                        Condimentos();
+                    }
+                    else if (desicion == 2)
+                    {
+                        Console.WriteLine("Excelente el Ticket de compra es el siguente:");
+                        Ticket();
+                    }
+
+
+                    break;
+
+
+
+
+
+                default:
+                    break;
+            }
+
+
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
